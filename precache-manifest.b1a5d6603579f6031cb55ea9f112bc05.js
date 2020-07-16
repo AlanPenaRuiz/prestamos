b53213c05ad172847e705677d509ec4d4b955e15
Alanhawk <alalanruiz@gmail.com>
2020-07-16 23:47:15 +0200

    Updates

---
+++ precache-manifest.b1a5d6603579f6031cb55ea9f112bc05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2bfca8b8105331f66e9a79c86804978",
+    "url": "/prestamos/index.html"
+  },
+  {
+    "revision": "e91e3291e0dff2757e86",
+    "url": "/prestamos/static/css/main.257a095d.chunk.css"
+  },
+  {
+    "revision": "68e09645afff03b820eb",
+    "url": "/prestamos/static/js/2.ede15c37.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prestamos/static/js/2.ede15c37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e91e3291e0dff2757e86",
+    "url": "/prestamos/static/js/main.90eff075.chunk.js"
+  },
+  {
+    "revision": "f78a5e4329a4403e6f6a",
+    "url": "/prestamos/static/js/runtime-main.03b10688.js"
+  }
+]);
